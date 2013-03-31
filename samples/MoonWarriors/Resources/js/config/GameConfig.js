6d6e35f1cebd1527a4c8e65207e6cbb17f51bcee
Vladislav <vladislav.lipatnikov@mail.ru>
2013-04-01 03:19:03 +0700

    First commit

---
+++ samples/MoonWarriors/Resources/js/config/GameConfig.js
@@ -0,0 +1,55 @@
+//life
+//MW.LIFE = 5;
+
+//score
+//MW.SCORE = 0;
+
+//sound
+MW.SOUND = true;
+
+//enemy move type
+MW.ENEMY_MOVE_TYPE = {};
+
+//delta x
+MW.DELTA_X = -100;
+
+//offset x
+MW.OFFSET_X = -24;
+
+//rot
+MW.ROT = -5.625;
+
+//bullet type
+MW.BULLET_TYPE = {
+    PLAYER:1,
+    ENEMY:2
+};
+
+//weapon type
+MW.WEAPON_TYPE = {
+    ONE:1
+};
+
+//unit tag
+MW.UNIT_TAG = {
+    ENMEY_BULLET:900,
+    PLAYER_BULLET:901,
+    ENEMY:1000,
+    PLAYER:1000
+};
+
+//attack mode
+MW.ENEMY_ATTACK_MODE = {
+    NORMAL:1,
+    TSUIHIKIDAN:2
+};
+
+//life up sorce
+MW.LIFEUP_SORCE = [50000, 100000, 150000, 200000, 250000, 300000];
+
+//container
+MW.CONTAINER = {
+    ENEMIES:[],
+    ENEMY_BULLETS:[],
+    PLAYER_BULLETS:[]
+};
